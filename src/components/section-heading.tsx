165f09d95f36e69d4b0837e2ea1ab632f4f74052
Tejas Solanki <tejassolanki176@gmail.com>
2025-08-12 05:51:29 +0000

    final commit

--- src/components/section-heading.tsx
+++ src/components/section-heading.tsx
@@ -8,7 +8,7 @@ type SectionHeadingProps = {
 export default function SectionHeading({ children, className }: SectionHeadingProps) {
   return (
     <div className="flex justify-center mb-12">
-        <h2 className={`text-3xl font-bold text-center text-primary capitalize font-headline sketch-border inline-block ${className}`}>
+        <h2 className={`text-3xl font-bold text-center text-primary capitalize font-headline border-b-2 border-accent pb-2 inline-block ${className}`}>
             {children}
         </h2>
     </div>
