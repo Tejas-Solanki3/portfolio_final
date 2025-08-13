8c341ecbb25f7b0f90d54e64707d7d970e2f5fd1
Tejas Solanki <tejassolanki176@gmail.com>
2025-08-13 06:38:25 +0000

    final commit

--- src/components/ui/lamp.tsx
+++ src/components/ui/lamp.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 import { useIsMobile } from "@/hooks/use-mobile";
@@ -12,6 +12,12 @@ export const LampContainer = ({
   className?: string;
 }) => {
   const isMobile = useIsMobile();
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <div
       className={cn(
@@ -21,63 +27,66 @@ export const LampContainer = ({
       )}
     >
       <div className="relative flex w-full flex-1 scale-y-125 items-center justify-center isolate z-0 ">
-        <motion.div
-          initial={{ opacity: 0.5, width: isMobile ? "7.5rem" : "15rem" }}
-          whileInView={{ opacity: 1, width: isMobile ? "15rem" : "30rem" }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          style={{
-            backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
-          }}
-          className="absolute inset-auto right-1/2 h-56 overflow-visible bg-gradient-conic from-purple-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
-        >
-          <div className="absolute  w-[100%] left-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
-          <div className="absolute  w-40 h-[100%] left-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0.5, width: isMobile ? "7.5rem" : "15rem" }}
-          whileInView={{ opacity: 1, width: isMobile ? "15rem" : "30rem" }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          style={{
-            backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
-          }}
-          className="absolute inset-auto left-1/2 h-56 bg-gradient-conic from-transparent via-transparent to-purple-500 text-white [--conic-position:from_290deg_at_center_top]"
-        >
-          <div className="absolute  w-40 h-[100%] right-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
-          <div className="absolute  w-[100%] right-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
-        </motion.div>
-        <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-slate-950 blur-2xl"></div>
-        <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
-        <div className="absolute inset-auto z-50 h-36 w-[14rem] md:w-[28rem] -translate-y-1/2 rounded-full bg-purple-500 opacity-50 blur-3xl"></div>
-        <motion.div
-          initial={{ width: isMobile ? "4rem" : "8rem" }}
-          whileInView={{ width: isMobile ? "8rem" : "16rem" }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          className="absolute inset-auto z-30 h-36 -translate-y-[7rem] rounded-full bg-purple-400 blur-2xl"
-        ></motion.div>
-        <motion.div
-          initial={{ width: isMobile ? "7.5rem" : "15rem" }}
-          whileInView={{ width: isMobile ? "15rem" : "30rem" }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          className="absolute inset-auto z-50 h-0.5 -translate-y-[3rem] bg-purple-400"
-        ></motion.div>
-
-        <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[8.5rem] bg-slate-950 "></div>
+        {isClient && (
+          <>
+            <motion.div
+              initial={{ opacity: 0.5, width: isMobile ? "7.5rem" : "15rem" }}
+              whileInView={{ opacity: 1, width: isMobile ? "15rem" : "30rem" }}
+              transition={{
+                delay: 0.3,
+                duration: 0.8,
+                ease: "easeInOut",
+              }}
+              style={{
+                backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
+              }}
+              className="absolute inset-auto right-1/2 h-56 overflow-visible bg-gradient-conic from-purple-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
+            >
+              <div className="absolute  w-[100%] left-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+              <div className="absolute  w-40 h-[100%] left-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0.5, width: isMobile ? "7.5rem" : "15rem" }}
+              whileInView={{ opacity: 1, width: isMobile ? "15rem" : "30rem" }}
+              transition={{
+                delay: 0.3,
+                duration: 0.8,
+                ease: "easeInOut",
+              }}
+              style={{
+                backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
+              }}
+              className="absolute inset-auto left-1/2 h-56 bg-gradient-conic from-transparent via-transparent to-purple-500 text-white [--conic-position:from_290deg_at_center_top]"
+            >
+              <div className="absolute  w-40 h-[100%] right-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
+              <div className="absolute  w-[100%] right-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+            </motion.div>
+            <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-slate-950 blur-2xl"></div>
+            <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
+            <div className="absolute inset-auto z-50 h-36 w-full max-w-[14rem] md:max-w-[28rem] -translate-y-1/2 rounded-full bg-purple-500 opacity-50 blur-3xl"></div>
+            <motion.div
+              initial={{ width: isMobile ? "4rem" : "8rem" }}
+              whileInView={{ width: isMobile ? "8rem" : "16rem" }}
+              transition={{
+                delay: 0.3,
+                duration: 0.8,
+                ease: "easeInOut",
+              }}
+              className="absolute inset-auto z-30 h-36 -translate-y-[7rem] rounded-full bg-purple-400 blur-2xl"
+            ></motion.div>
+            <motion.div
+              initial={{ width: isMobile ? "7.5rem" : "15rem" }}
+              whileInView={{ width: isMobile ? "15rem" : "30rem" }}
+              transition={{
+                delay: 0.3,
+                duration: 0.8,
+                ease: "easeInOut",
+              }}
+              className="absolute inset-auto z-50 h-0.5 -translate-y-[3rem] bg-purple-400"
+            ></motion.div>
+            <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[8.5rem] bg-slate-950 "></div>
+          </>
+        )}
       </div>
 
       <div className="relative z-50 flex -translate-y-90 flex-col items-center px-5">
