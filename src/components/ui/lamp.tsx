f65de4b89389e8d61a9153d8cf53874ef348c9a3
Tejas Solanki <tejassolanki176@gmail.com>
2025-08-13 13:03:50 +0000

    final commit

--- src/components/ui/lamp.tsx
+++ src/components/ui/lamp.tsx
@@ -65,7 +65,7 @@ export const LampContainer = ({
               <div className="absolute w-[100%] right-0 bg-background h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
             </motion.div>
 
-            <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-background blur-2xl"></div>
+            <div className="absolute top-1/2 h-48 w-full scale-x-150 bg-background blur-2xl"></div>
             <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
             <motion.div
               initial={{ width: isMobile ? "8rem" : "16rem" }}
