1c761cc2ca750ce5aff5d64ef4d818c24c7517e5
Tejas Solanki <tejassolanki176@gmail.com>
2025-08-13 13:11:31 +0000

    final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,17 @@ import AboutMe from '@/components/about-me';
 import Contact from '@/components/contact';
 import Footer from '@/components/footer';
 import Expertise from '@/components/expertise';
+import { LampContainer } from '@/components/ui/lamp';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1">
-        <LandingPage />
-        <AboutMe />
+        <LampContainer>
+          <LandingPage />
+          <AboutMe />
+        </LampContainer>
         <Expertise />
         <Projects />
         <Gallery />
